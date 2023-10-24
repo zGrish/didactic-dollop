a720a0fe9f427f785b96e051fec62615b0683c17
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-10-24 23:27:33 +0530

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,8 @@
+cubes = []       
+print('Printing the list:')         
+for cube in range(1, 11):
+    cubes.append(cube**3)                # joinning new iterations to the list using append()
+print(cubes)                      
+print('\nPrinting individual values:')
+for cube in range(1, 11):
+    print(cube)                   
