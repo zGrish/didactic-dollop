bb472d8ae1ae039655c2a7925d14fd1b978d9bde
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-10-24 23:00:23 +0530

    Add files via upload

---
+++ stuff_with_lists/1to1million.py
@@ -0,0 +1,8 @@
+numbers = list(range(1, 1000001))
+print('Numbers from 1 to 1 million:')
+for number in numbers:
+    print(number)
+print(f'List of numbers from 1 to 1 million: {numbers}')
+print(f'Smallest number: {min(numbers)}')
+print(f'Largest number: {max(numbers)}')
+print(f'Sum of the numbers in the list: {sum(numbers)}')
