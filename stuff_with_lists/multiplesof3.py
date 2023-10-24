0c9215d84be21cb84a76047d86acfccb7bb40b26
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-10-24 23:16:36 +0530

    Add files via upload

---
+++ stuff_with_lists/multiplesof3.py
@@ -0,0 +1,8 @@
+print('With for-loop:')
+multiples = list(range(3, 31, 3)) 
+for multiple in multiples:
+    print(multiple)
+print(f'\n')
+print('Without for-loop:') 
+multiples = list(range(3, 31, 3))
+print(multiples)
