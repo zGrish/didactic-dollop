33d24c9f7c4a4845c916393ac4d6362b6d5d01c2
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-11-15 22:49:42 +0530

    Add files via upload

---
+++ stuff_with_while/infiniteloop.py
@@ -0,0 +1,6 @@
+# Warning trigger INFINITE LOOP
+## Get ready to press CTRL + C
+message = input("Enter a message that you'd not mind haunting you for a while:\n")
+i = 0.0000000000001
+while i > 0:
+    print(message)
