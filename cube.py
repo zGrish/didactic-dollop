46f4e582ad86436ba92d057c3f594f14ddde79f4
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-11-23 22:00:24 +0530

    Delete cube.py

--- cube.py
+++
@@ -1,8 +0,0 @@
-cubes = []       
-print('Printing the list:')         
-for cube in range(1, 11):
-    cubes.append(cube**3)                # joinning new iterations to the list using append()
-print(cubes)                      
-print('\nPrinting individual values:')
-for cube in range(1, 11):
-    print(cube)                   
