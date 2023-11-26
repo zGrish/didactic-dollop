3e66f7d36c80c69946e389c0f5b53aa8c2f4ef43
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-11-26 22:02:29 +0530

    Update 2_arguments.py

--- stuff_with_functions/2_arguments.py
+++ stuff_with_functions/2_arguments.py
@@ -1,14 +1,16 @@
-def make_shirt(size, quote = 'percy jackson'):
+def make_shirt(size = 'l', quote = 'percy jackson'):
     """Display t-shirt size and 
     custom quote to be printed."""
     print(f"Size: {size.upper()}"
           f"\nQuote: {quote.title()}\n")      # multi-line print statement
     
-# positional arguments
+# positional arguments (ignores default values, assigned left-to-right)
 make_shirt('xs', 'star wars')
 
-# keyword arguments
+# keyword arguments (ignores default, assigned as mentioned)
 make_shirt(quote = 'harry potter', size = 'xxl')
 
 # positional and keyword arguments (activating default value)
 make_shirt('m')
+make_shirt(quote = 'i love python')
+make_shirt(size = 'xl')
