3ffda00434f36dce2c7d959c216881de03ab59ec
Grishma Nair <139485160+zGrish@users.noreply.github.com>
2023-11-27 22:08:55 +0530

    Create factorial.py

---
+++ stuff_with_numbers/factorial.py
@@ -0,0 +1,18 @@
+def factorial(numf):
+    fact = 1
+    for i in range(1, numf + 1):
+        fact = fact * i
+    return fact
+
+# USING RECURSION
+    # def factorial(numf):
+        # if numf == 0:
+        #     return 1
+        # else:
+        #     return numf * factorial(numf - 1)
+
+def main():
+    num = int(input("\nEnter any number: "))
+    print(f"The factorial of {num} is {factorial(num)}\n")
+    
+main()
